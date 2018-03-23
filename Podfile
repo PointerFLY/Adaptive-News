8066c195c1c0370148b53fd68822f0a4833f8dba
PointerFLY <linghao.michael.ma@gmail.com>
2018-03-23 12:30:36 +0000

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '9.0'
+use_frameworks!
+# inhibit_all_warnings!
+
+target 'hawkEye' do
+    pod 'Alamofire', '~> 4.7'
+    pod 'RealmSwift', '~> 3.1'
+    pod 'SwiftyUserDefaults', '~> 3.0.1'
+    pod 'Log', '~> 1.0'
+    pod 'SVProgressHUD', '~> 2.2.5'
+    pod 'SnapKit', '~> 4.0.0'
+    pod 'BlocksKit', '~> 2.2.5'
+end
---
+++ hawkEye/RootViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RootViewController.swift
+//  hawkEye
+//
+//  Created by PointerFLY on 23/03/2018.
+//  Copyright © 2018 PointerFLY. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil);
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
