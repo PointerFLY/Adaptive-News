9e84193926436d36718aec608931bde2d4caeeaf
PointerFLY <linghao.michael.ma@gmail.com>
2018-03-24 12:14:31 +0000

    Restruct to folders

---
+++ hawkEye/User Interface/Profile/ProfileViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ProfileViewController.swift
+//  hawkEye
+//
+//  Created by PointerFLY on 23/03/2018.
+//  Copyright © 2018 PointerFLY. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UITableViewController {
+    
+    init() {
+        super.init(style: .grouped)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        switch section {
+        case 2:
+            let view = UIView()
+            view.backgroundColor = UIColor.red
+            return view
+        default:
+            return nil
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        switch section {
+        case 2:
+            return 120.0
+        default:
+            return UITableViewAutomaticDimension
+        }
+    }
+}
